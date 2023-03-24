7b01fb310c0ee33c95067dcb82109e89171ace50
Vikram Aiyer <103357386+vikram8621@users.noreply.github.com>
2023-03-24 13:42:21 +0530

    Add files via upload

--- mavenpizza.sql
+++ mavenpizza.sql
@@ -6,6 +6,7 @@ SELECT * FROM pizza_types;
 SELECT * FROM pizzas;
 
 
+/* Calculate Total orders and average price by Pizza types  */
 SELECT
 pizza_type_id,
 AVG(price) AS average_price,
@@ -21,7 +22,7 @@ GROUP BY pizza_type_id
 ORDER BY price DESC;
 
 
-
+/* Calculate Total revenue and average price by Pizza types  */
 SELECT 
 pizza_type_id,
 MAX(average_price) AS max_avg_price,
@@ -45,6 +46,7 @@ GROUP BY pizza_type_id
 AS rev_avg_totals
 GROUP BY pizza_type_id;
 
+/* Create a Stored procedure as per previous question */
 CREATE PROCEDURE sp_AvgpizzaValueOrders()
 BEGIN
 
@@ -75,18 +77,41 @@ END;
 CALL sp_AvgpizzaValueOrders();
 
 
-
+/* Calculate total pizza orders by size */
 SELECT
-pizza_type_id,
-AVG(price) AS average_price,
-SUM(price) AS total_revenue,
-COUNT(DISTINCT order_details.order_id) AS no_of_orders
-FROM 
-pizzas
-LEFT JOIN order_details
-ON 
-pizzas.category_id = order_details.category_id
-WHERE price >0
-AND quantity >0
-GROUP BY pizza_type_id
-ORDER BY price DESC;
+pizza_id,
+COUNT(*) AS no_of_orders_by_size
+FROM order_details
+GROUP BY pizza_id;
+
+/* Window Function 1
+Calculate total order quantiity by pizza_id, category_id */
+SELECT
+DISTINCT(pizza_id),
+category_id,
+SUM(quantity)
+OVER (
+PARTITION BY category_id
+ORDER BY pizza_id
+RANGE UNBOUNDED PRECEDING) total
+FROM
+order_details;
+
+/* Window Function 2
+Rank pizzas by order quantity and having orders not more than 3*/
+SELECT 
+DISTINCT(pizza_id),
+category_id,
+RANK() OVER pizza_sold_avg
+FROM
+order_details
+WHERE quantity BETWEEN 1 AND 3
+GROUP BY pizza_id,
+category_id
+HAVING SUM(quantity) < 1000
+WINDOW pizza_sold_avg AS (
+PARTITION BY pizza_id
+ORDER BY avg(quantity) DESC)
+ORDER BY 
+pizza_id,
+category_id;
